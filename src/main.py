4e3c42fb255ca18d8bc79d1006c8ddbeb100aea0
Rusky <iamayaanalee@gmail.com>
2024-10-30 20:16:25 +0500

    ⚙️ rewrite of the entire code

--- src/main.py
+++ src/main.py
@@ -1,36 +1,37 @@
 import os
-import sys
 import json
 import asyncio
 import platform
 import requests
 import websockets
 from colorama import init, Fore
-from keep_alive import keep_alive
-
 init(autoreset=True)
 
-status = "online"  # online/dnd/idle
-custom_status = "youtube.com/@SealedSaucer"  # Custom Status
+with open('settings.json') as settings_file:
+    settings = json.load(settings_file)
 
-usertoken = os.getenv("TOKEN")
-if not usertoken:
-    print(f"{Fore.WHITE}[{Fore.RED}-{Fore.WHITE}] Please add a token inside Secrets.")
-    sys.exit()
+TOKEN = settings["TOKEN"]
+if not TOKEN:
+    print(f"{Fore.WHITE}[{Fore.LIGHTRED_EX}ERROR{Fore.WHITE}] We couldn't find a token inside settings.json Please add a token inside settings.json")
+    input()
 
-headers = {"Authorization": usertoken, "Content-Type": "application/json"}
+status_texts = settings["STATUS_TEXT"]
+status_rotation_seconds = int(settings.get("STATUS_TEXT_ROTATION_SECONDS", 300))
+status = settings.get("STATUS_TYPE", "online")
 
-validate = requests.get("https://canary.discordapp.com/api/v9/users/@me", headers=headers)
-if validate.status_code != 200:
-    print(f"{Fore.WHITE}[{Fore.RED}-{Fore.WHITE}] Your token might be invalid. Please check it again.")
-    sys.exit()
+headers = {"Authorization": TOKEN, "Content-Type": "application/json"}
+
+check_token = requests.get("https://canary.discordapp.com/api/v9/users/@me", headers=headers)
+if check_token.status_code != 200:
+    print(f"{Fore.WHITE}[{Fore.LIGHTRED_EX}ERROR{Fore.WHITE}] The token you provided might be invalid Please double check the token you entered in settings.json")
+    input()
 
 userinfo = requests.get("https://canary.discordapp.com/api/v9/users/@me", headers=headers).json()
 username = userinfo["username"]
 discriminator = userinfo["discriminator"]
 userid = userinfo["id"]
 
-async def onliner(token, status):
+async def Main(token, status, custom_status):
     async with websockets.connect("wss://gateway.discord.gg/?v=9&encoding=json") as ws:
         start = json.loads(await ws.recv())
         heartbeat = start["d"]["heartbeat_interval"]
@@ -59,14 +60,8 @@ async def onliner(token, status):
                         "state": custom_status,
                         "name": "Custom Status",
                         "id": "custom",
-                                #Uncomment the below lines if you want an emoji in the status
-                                #"emoji": {
-                                    #"name": "emoji name",
-                                    #"id": "emoji id",
-                                    #"animated": False,
-                                #},
-                            }
-                        ],
+                    }
+                ],
                 "status": status,
                 "afk": False,
             },
@@ -74,18 +69,25 @@ async def onliner(token, status):
         await ws.send(json.dumps(cstatus))
 
         online = {"op": 1, "d": "None"}
-        await asyncio.sleep(heartbeat / 1000)
-        await ws.send(json.dumps(online))
+        
+        if len(json.dumps(online)) > 1048576:
+            return
+        else:
+            await ws.send(json.dumps(online))
 
-async def run_onliner():
+        await asyncio.sleep(heartbeat / 1000)
+
+async def Run():
     if platform.system() == "Windows":
         os.system("cls")
     else:
         os.system("clear")
-    print(f"{Fore.WHITE}[{Fore.LIGHTGREEN_EX}+{Fore.WHITE}] Logged in as {Fore.LIGHTBLUE_EX}{username} {Fore.WHITE}({userid})!")
+    print(f"{Fore.WHITE}[{Fore.LIGHTGREEN_EX}INFO{Fore.WHITE}] Logged in as {Fore.LIGHTGREEN_EX_EX}{username} {Fore.WHITE}({userid})")
+    current_status_index = 0
     while True:
-        await onliner(usertoken, status)
-        await asyncio.sleep(50)
+        custom_status = status_texts[current_status_index]
+        await Main(TOKEN, status, custom_status)
+        current_status_index = (current_status_index + 1) % len(status_texts)
+        await asyncio.sleep(status_rotation_seconds)
 
-keep_alive()
-asyncio.run(run_onliner())
+asyncio.run(Run())
